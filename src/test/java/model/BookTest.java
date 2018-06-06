5c21d6a37b46bf1be01ece81c113d5329fc88c00
Sabrina Perardt <sabrinaperardt@DANs-MBP.hitronhub.home>
2018-06-06 16:59:09 -0700

    TDD second round for Book Class

--- src/test/java/model/BookTest.java
+++ src/test/java/model/BookTest.java
@@ -1,6 +1,20 @@
 package model;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
-public class BookTest{
+public class BookTest {
 	Book book;
+
+	@Before
+	public void setUp() {
+		book = new Book();
+	}
+
+	@Test
+	public void booksNameTest() {
+		String name = "Harry Potter";
+		Assert.assertEquals(name, book.getName());
+	}
 }
