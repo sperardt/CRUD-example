9d13aff71ea8e923275c75eb5171d1c7779dbe07
Sabrina Perardt <sabrinaperardt@DANs-MBP.hitronhub.home>
2018-06-06 17:09:41 -0700

    TDD - Book Class 4th round and Test is green

--- src/main/java/model/Book.java
+++ src/main/java/model/Book.java
@@ -3,6 +3,7 @@ package model;
 public class Book {
 
 	private String name;
+	private String id;
 
 	public String getName() {
 		return name;
@@ -10,5 +11,13 @@ public class Book {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
 	}
 }
