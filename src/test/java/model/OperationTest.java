e57b5949ca65c2d35237655a3b7ad788d8b0dd98
Sabrina Perardt <sabrinaperardt@DANs-MBP.hitronhub.home>
2018-06-15 15:37:01 -0700

    TDD - third round OperationTest - green

--- src/test/java/model/OperationTest.java
+++ src/test/java/model/OperationTest.java
@@ -2,6 +2,8 @@ package model;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -11,9 +13,24 @@ public class OperationTest {
 	@Before
 	public void setUp() {
 		op = new Operation();
+		
 	}
+	
 	@Test
-	public void booksNameTest() {
+	public void getBooksTest() {
 		assertNull(Operation.getBookList());
+		
+		ArrayList<Book> bookList = new ArrayList<Book>();
+		Book book = new Book();
+		
+		book.setId("1");
+		book.setName("Harry Potter");
+		book.setNumberPages(500);
+		book.setPrice(10.00);
+		
+		bookList.add(book);
+		Operation.setBookList(bookList);
+		
+		assertEquals(1, Operation.getBookList().size());
 	}
 }
