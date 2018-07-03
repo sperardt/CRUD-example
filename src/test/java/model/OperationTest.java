0b521df10919d293ca05cdba5209131e54b2f121
Sabrina Perardt <sabrinaperardt@DANs-MBP.hitronhub.home>
2018-07-03 16:37:46 -0700

    refactoring - changing type of bookList - before ArrayList, now Map/HashMap

--- src/test/java/model/OperationTest.java
+++ src/test/java/model/OperationTest.java
@@ -3,17 +3,19 @@ package model;
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.junit.Before;
 import org.junit.Test;
 
 public class OperationTest {
-	ArrayList<Book> bookList;
+	Map<String, Book> bookList;
 	Book book;
 
 	@Before
 	public void setUp() {
-		bookList = new ArrayList<Book>();
+		bookList = new HashMap<String, Book>();
 		book = new Book();
 		
 		book.setId("1");
@@ -21,14 +23,25 @@ public class OperationTest {
 		book.setNumberPages(500);
 		book.setPrice(10.00);
 		
-		bookList.add(book);
+		bookList.put("1", book);
 	}
 	
 	@Test
-	public void getBooksTest() {
-		assertNull(Operation.getBookList());
-
-		Operation.setBookList(bookList);	
-		assertEquals(1, Operation.getBookList().size());
+	public void addBooksTest() {
+		//assertNull(Operation.getBookList());
+		assertTrue(bookList.isEmpty());
+//		Operation.setBookList(bookList);	
+//		assertEquals(1, Operation.getBookList().size());
+//		
+//		book.setId("2");
+//		book.setName("Hunger games");
+//		book.setNumberPages(300);
+//		book.setPrice(15.00);
+//		
+//		bookList.add(book);
+//		Operation.setBookList(bookList);	
+//		
+//		assertEquals(2, Operation.getBookList().size());
 	}
+	
 }
